39da68f0776b7c9299d5caf1bb1c29de088b1c71
Tianyu <55935965+Tianyu-Fang@users.noreply.github.com>
2024-09-30 10:45:06 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include"MLFQ.h"
+using namespace std;
+
+//main program
+int main()
+{
+    MLFQ obj;								//create object of MLFQ
+    obj.setFileName("input2.txt");			//set file name for input data
+    obj.start();							// call the start() method of MLFQ, which will perform MLFQ
+    return 0;								// end of main program
+}
